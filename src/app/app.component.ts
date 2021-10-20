3c234ed8eff9d5a8faaed88572212d2e6e364b9b
rehanhussainimam <rehanhussainimam@gmail.com>
2021-10-20 13:31:44 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export interface tablevalue {
 }
 
 export interface user {
+  position: number;
   name: tablevalue;
   age: tablevalue;
   edu: tablevalue;
@@ -45,24 +46,44 @@ export class AppComponent {
   public currentExpandedRow: any;
   public expandRow: boolean = false;
   isExpansionDetailRow = (_, row: user) => row.hasOwnProperty('detailRow');
+  explansionDetialRowCollection = new Array<any>();
 
   constructor() {
     // this.dataSource = this.userdata;
+    this.dataSource = new ExampleDataSource();
+  }
+
+  public toggleDetailsRow(row: user): void {
+    this.expandRow = this.explansionDetialRowCollection.includes(row);
+    if (this.expandRow !== true) {
+      this.explansionDetialRowCollection.push(row);
+    } else {
+      // let index = this.explansionDetialRowCollection.findIndex(idRow => idRow.name === row.element.name);
+      let test = this.explansionDetialRowCollection[0].name;
+      this.explansionDetialRowCollection.forEach((item, index) => {
+        if (item.position === row.position)
+          this.explansionDetialRowCollection.splice(index, 1);
+      });
+      // this.explansionDetialRowCollection.splice(0, 1);
+    }
   }
 }
 
 const userdata: user[] = [
   {
+    position: 1,
     name: { value: 'Rehan' },
     age: { value: '28' },
     edu: { value: 'Maths' },
   },
   {
+    position: 2,
     name: { value: 'Swami' },
     age: { value: '30' },
     edu: { value: 'Sciencee' },
   },
   {
+    position: 3,
     name: { value: 'Sunil' },
     age: { value: '26' },
     edu: { value: 'History' },
