536b9f4dda39e0239a064efc94a82dd8958b3737
rehanhussainimam <rehanhussainimam@gmail.com>
2021-10-20 13:27:46 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 export interface tablevalue {
@@ -15,30 +22,47 @@ export interface user {
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', visibility: 'hidden' })
+      ),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
-  public userdata: user[] = [
-    {
-      name: { value: 'Rehan' },
-      age: { value: '28' },
-      edu: { value: 'Maths' },
-    },
-    {
-      name: { value: 'Swami' },
-      age: { value: '30' },
-      edu: { value: 'Sciencee' },
-    },
-    {
-      name: { value: 'Sunil' },
-      age: { value: '26' },
-      edu: { value: 'History' },
-    },
-  ];
-
   dataSource;
   displayedColumns = ['name', 'age', 'edu'];
 
+  public currentExpandedRow: any;
+  public expandRow: boolean = false;
+  isExpansionDetailRow = (_, row: user) => row.hasOwnProperty('detailRow');
+
   constructor() {
-    this.dataSource = this.userdata;
+    // this.dataSource = this.userdata;
   }
 }
+
+const userdata: user[] = [
+  {
+    name: { value: 'Rehan' },
+    age: { value: '28' },
+    edu: { value: 'Maths' },
+  },
+  {
+    name: { value: 'Swami' },
+    age: { value: '30' },
+    edu: { value: 'Sciencee' },
+  },
+  {
+    name: { value: 'Sunil' },
+    age: { value: '26' },
+    edu: { value: 'History' },
+  },
+];
--- src/app/sub-table/sub-table.component.ts
+++ src/app/sub-table/sub-table.component.ts
@@ -1,9 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-sub-table',
   templateUrl: 'sub-table.component.html',
   styleUrls: ['sub-table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', visibility: 'hidden' })
+      ),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class SubTableComponent implements OnInit {
   @Input() data: any;
